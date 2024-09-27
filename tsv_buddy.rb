7a28844f46388262165a9d818948539ebb4b0bff
root <root@LAPTOP-O299TGTI.>
2024-09-27 12:14:53 +0800

    8 runs, 14 assertions, 0 failures, 0 errors, 0 skips

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -2,17 +2,46 @@
 
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
-  # Converts a String with TSV data into internal data structure @data
-  # arguments: tsv - a String in TSV format
-  # returns: nothing
-  def take_tsv(tsv)
+  TAB = "\t"
+  NEWLINE = "\n"
+  NIL = ''
+  attr_reader :data
 
+  def take_tsv(tsv)
+    lines = tsv.split(NEWLINE)
+    headers = extract_headers(lines[0])
+    @data = parse_rows(lines[1..], headers)
   end
 
-  # Converts @data into tsv string
-  # arguments: none
-  # returns: String in TSV format
-  def to_tsv
+  def to_tsv(include_header: true)
+    return 'No data available' if @data&.empty?
 
+    headers = include_header ? "#{@data.first.keys.join(TAB)}\n" : NIL
+    rows = @data.map { |record| record.values.join(TAB) }
+    "#{headers}#{rows.join(NEWLINE)}\n"
+  end
+
+  private
+
+  def extract_headers(header_line)
+    headers = header_line.strip.split(TAB)
+    raise 'No headers found' if headers.empty?
+
+    headers
+  end
+
+  def parse_rows(lines, headers)
+    lines.map { |row| tsv_row_to_hash(row, headers) }
+  end
+
+  def tsv_row_to_hash(row, headers)
+    values = row.strip.split(TAB)
+    build_hash(headers, values)
+  end
+
+  def build_hash(headers, values)
+    headers.each_with_index.with_object({}) do |(header, index), hash|
+      hash[header] = values[index] || NIL
+    end
   end
 end
